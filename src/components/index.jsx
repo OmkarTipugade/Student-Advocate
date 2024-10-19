94843e1958c705b9a2c376fc21565169368e2a77
Omkar Tipugade <omtipugade3904@gmail.com>
2024-10-19 20:45:53 +0530

    Sing up and Login page final commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import LoginForm from "./LoginForm";
-// import SignUpForm from "./SignUpForm";
+
 const index = () => {
-  return (
-    <div>
-      <LoginForm />
-      {/* <SignUpForm /> */}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default index;
